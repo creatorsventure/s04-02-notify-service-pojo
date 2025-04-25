1ae25597ef9926032d51f469f77b20e876bd6c2d
ram <ram_r@live.com>
2025-04-25 10:54:07 +0400

    COMMIT 25-04-2025

--- src/main/java/com/cv/s0402notifyservicepojo/helper/NotifyHelper.java
+++ src/main/java/com/cv/s0402notifyservicepojo/helper/NotifyHelper.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Locale;
 
-public class NotifiyHelper implements Serializable {
+public class NotifyHelper implements Serializable {
     @Serial
     private static final long serialVersionUID = 3206950347931949730L;
 
@@ -42,7 +42,7 @@ public class NotifiyHelper implements Serializable {
                                 .build(),
                         ContentDto.builder()
                                 .translate(true)
-                                .keyOrContent("email.activate-account.hint-2")
+                                .keyOrContent("email.general.warning")
                                 .build(),
                         ContentDto.builder()
                                 .translate(true)
@@ -77,11 +77,80 @@ public class NotifiyHelper implements Serializable {
                                 .build(),
                         ContentDto.builder()
                                 .translate(true)
-                                .keyOrContent("email.reset-password.hint-2")
+                                .keyOrContent("email.general.warning")
                                 .build(),
                         ContentDto.builder()
                                 .translate(true)
                                 .keyOrContent("email.general.link-expiry")
+                                .build()
+                )).build(), recipientDto);
+        return notifyDto;
+    }
+
+    public static NotifyDto notifyAccountSignUp(RecipientDto recipientDto, Locale locale, String link, String id) {
+        NotifyDto notifyDto = new NotifyDto();
+        notifyDto.mapSingle(MessageDto.builder()
+                .deliveryChannel(DeliveryChannel.EMAIL)
+                .deliveryTemplate(DeliveryTemplate.BASE_LAYOUT)
+                .locale(locale)
+                .trackId(id)
+                .attachment(false)
+                .table(false)
+                .subjectDto(ContentDto.builder()
+                        .translate(true)
+                        .keyOrContent("email.account.sign-up.subject")
+                        .build())
+                .contentLines(List.of(
+                        ContentDto.builder()
+                                .translate(true)
+                                .keyOrContent("email.account.sign-up.hint-1")
+                                .build(),
+                        ContentDto.builder()
+                                .link(true)
+                                .keyOrLinkLabel("email.general.link-click")
+                                .keyOrContent(link)
+                                .build(),
+                        ContentDto.builder()
+                                .translate(true)
+                                .keyOrContent("email.general.warning")
+                                .build(),
+                        ContentDto.builder()
+                                .translate(true)
+                                .keyOrContent("email.general.link-expiry")
+                                .build()
+                )).build(), recipientDto);
+        return notifyDto;
+    }
+
+    public static NotifyDto notifyOTP(RecipientDto recipientDto, Locale locale, String otp, String id) {
+        NotifyDto notifyDto = new NotifyDto();
+        notifyDto.mapSingle(MessageDto.builder()
+                .deliveryChannel(DeliveryChannel.EMAIL)
+                .deliveryTemplate(DeliveryTemplate.BASE_LAYOUT)
+                .locale(locale)
+                .trackId(id)
+                .attachment(false)
+                .table(false)
+                .subjectDto(ContentDto.builder()
+                        .translate(true)
+                        .keyOrContent("email.otp.subject")
+                        .build())
+                .contentLines(List.of(
+                        ContentDto.builder()
+                                .translate(true)
+                                .keyOrContent("email.otp.hint-1")
+                                .build(),
+                        ContentDto.builder()
+                                .translate(false)
+                                .keyOrContent(otp)
+                                .build(),
+                        ContentDto.builder()
+                                .translate(true)
+                                .keyOrContent("email.general.warning")
+                                .build(),
+                        ContentDto.builder()
+                                .translate(true)
+                                .keyOrContent("email.otp.link-expiry")
                                 .build()
                 )).build(), recipientDto);
         return notifyDto;
